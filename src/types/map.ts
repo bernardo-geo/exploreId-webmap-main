12b39bd7f41400d31d04d3eeb82929902992d2c6
bernardo-geo <lbernardo19@gmail.com>
2024-11-26 10:28:41 +0000

    Add files via upload

--- src/types/map.ts
+++ src/types/map.ts
@@ -9,14 +9,25 @@ export type BaseMaps = {
 
 export const baseMaps: BaseMaps = {
   standard: {
-    url: 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+    url: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>'
   },
-  streets: {
+  street: {
     url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   },
-  satellite: {
+  satélite: {
+    url: 'https://api.maptiler.com/maps/hybrid/256/{z}/{x}/{y}.jpg?key=1lbRLmDDEyGexHFbqHPd',
+    attribution: '&copy; <a href="https://www.maptiler.com/">MapTiler</a> contributors'
+  }
+};
+
+  /*standard: {
+    url: 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+  },*/
+
+  /*satellite: {
     url: 'https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}' +
       '&style=feature:administrative|visibility:off' +
       '&style=feature:poi|visibility:off' +
@@ -26,15 +37,4 @@ export const baseMaps: BaseMaps = {
       '&style=feature:poi|element:geometry|visibility:off' +
       '&style=feature:poi.business|element:geometry|visibility:off',
     attribution: '&copy; <a href="https://www.google.com/permissions/geoguidelines/">Google Maps</a>'
-  },
-  voyager: {
-    url: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>'
-  },
-  mapTiler: {
-    url: 'https://api.maptiler.com/maps/hybrid/256/{z}/{x}/{y}.jpg?key=1lbRLmDDEyGexHFbqHPd',
-    attribution: '&copy; <a href="https://www.maptiler.com/">MapTiler</a> contributors'
-  }
-  
-  
-};
+  },*/
