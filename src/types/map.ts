d8112d68c0e9a82f37da17b661dd549275071844
bernardo-geo <lbernardo19@gmail.com>
2024-11-26 10:35:01 +0000

    Add files via upload

--- src/types/map.ts
+++ src/types/map.ts
@@ -8,17 +8,17 @@ export type BaseMaps = {
 };
 
 export const baseMaps: BaseMaps = {
-  standard: {
+  base: {
     url: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>'
-  },
-  street: {
-    url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   },
   satélite: {
     url: 'https://api.maptiler.com/maps/hybrid/256/{z}/{x}/{y}.jpg?key=1lbRLmDDEyGexHFbqHPd',
     attribution: '&copy; <a href="https://www.maptiler.com/">MapTiler</a> contributors'
+  },
+  urbano: {
+    url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }
 };
 
